b0ef626bd57c516002fa30ba7ee78d59a9d432fb
Elton Mozart <elton.mozart@gmail.com>
2024-06-17 07:49:00 -0300

    Testes e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../prisma/prisma.service';
 
-describe('AppController (e2e)', () => {
+describe('Recebedores (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +14,109 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prisma = moduleFixture.get(PrismaService);
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/recebedor (POST)', async () => {
+    const payload = {
+      nome: 'João da Silva',
+      email: 'test.e2e@teste.com',
+      cpfCnpj: '11122233300',
+      tipoChave: 'CPF',
+      chave: '11122233300',
+    };
+    const response = await request(app.getHttpServer())
+      .post('/recebedor')
+      .send(payload);
+
+    expect(response.statusCode).toBe(201);
+
+    const result = await prisma.recebedor.findUnique({
+      where: { id: response.body.id },
+    });
+
+    expect(result.nomeRasaoSocial).toBe(payload.nome);
+    expect(result.email).toBe(payload.email);
+    expect(result.cpfCnpj).toBe(payload.cpfCnpj);
+    expect(result.tipoChave).toBe(payload.tipoChave);
+    expect(result.chave).toBe(payload.chave);
+  });
+
+  it('/recebedor/list (POST)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/recebedor/list')
+      .send({
+        pagina: 1,
+        itens: 5,
+      });
+
+    expect(response.statusCode).toBe(201);
+  });
+
+  it('/recebedor (GET)', async () => {
+    const id = 'c005782d-282c-4427-810a-3a7719734d94';
+    const response = await request(app.getHttpServer()).get(`/recebedor/${id}`);
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('/recebedor (PATCH) - id inválido', async () => {
+    const id = 'id_invalido';
+    const response = await request(app.getHttpServer())
+      .patch(`/recebedor/${id}`)
+      .send({
+        nome: 'João de Souza',
+        email: 'test.e2e.alterado@teste.com.XXX',
+        cpfCnpj: '44455566699',
+        tipoChave: 'CPF',
+        chave: '44455566699',
+      });
+
+    expect(response.statusCode).toBe(404);
+  });
+
+  it('/recebedor (PATCH) - id válido', async () => {
+    const id = 'c005782d-282c-4427-810a-3a7719734d94';
+    const response = await request(app.getHttpServer())
+      .patch(`/recebedor/${id}`)
+      .send({
+        nome: 'João de Souza',
+        email: 'test.e2e.alterado@teste.com.br',
+        cpfCnpj: '44455566699',
+        tipoChave: 'CPF',
+        chave: '44455566699',
+      });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('/recebedor (PATCH) - Status Validado', async () => {
+    const id = '4249d450-4524-4c4e-96a5-3331b49f4757';
+    const response = await request(app.getHttpServer())
+      .patch(`/recebedor/${id}`)
+      .send({
+        nome: 'João dos Santos',
+        email: 'test.e2e.alterado@teste.com.br',
+        cpfCnpj: '77788899900',
+        tipoChave: 'CPF',
+        chave: '77788899900',
+      });
+
+    expect(response.statusCode).toBe(403);
+  });
+
+  it('/recebedor (DELETE)', async () => {
+    const id = '07c08e08-58e6-4234-b35b-e53097311842';
+    const response = await request(app.getHttpServer()).delete(
+      `/recebedor/${id}`,
+    );
+
+    expect(response.statusCode).toBe(204);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
